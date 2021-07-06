d16865d246c5c5395f04a9615c0ee9ed2467bdc6
DanielAristy <danielaristy22@gmail.com>
2021-07-06 14:38:11 -0500

    Base inicial del proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.css'
+import { _getPhones } from './utils/_DATA'
+import { receivePhones } from './actions/phones'
+import Container from './components/container'
+
+
+class App extends Component {
+  componentDidMount() {
+    _getPhones()
+      .then((phones) => {
+        this.props.dispatch(receivePhones(phones))
+      })
+  }
+  render() {
+    const { phones } = this.props
+
+    return (
+      <Container phones={phones} />
+    );
+  }
 }
 
-export default App;
+function mapStateToProps({ phones }) {
+  return {
+    phones
+  }
+}
+
+export default connect(mapStateToProps)(App);
